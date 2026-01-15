26731c9f095ad2644e7be8b8a3031ae53437d9b9
Mark Fairhurst <mark@fairhursts.net>
2026-01-15 21:46:28 +0000

    chore: Remove unused import

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -8,7 +8,6 @@ import {
   BreadcrumbSeparator,
 } from '@/components/ui/breadcrumb'
 import { Button } from '@/components/ui/button'
-import { Separator } from '@/components/ui/separator'
 import { SidebarTrigger } from '@/components/ui/sidebar'
 
 import ProfileDropdown from '@/components/shadcn-studio/blocks/dropdown-profile'
